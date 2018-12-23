3c5484de6a487626f9b8256c099341f9c086eea7
beacursounity <43536093+beacursounity@users.noreply.github.com>
2018-12-23 14:01:20 +0100

    Initial commit

---
+++ Assets/_GameAssets/Scripts/GameOverScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameOverScript : MonoBehaviour {
+
+
+    public void BotonReset() {
+        SceneManager.LoadScene(0);
+    }
+}
